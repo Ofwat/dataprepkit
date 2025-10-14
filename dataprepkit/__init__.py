474b7ff4cdfe387f2f10ada9dba15ae4af49e488
coau <79207858+coau@users.noreply.github.com>
2025-10-14 19:30:41 +0100

    add basic dim table etl

--- dataprepkit/__init__.py
+++ dataprepkit/__init__.py
@@ -1,6 +1,4 @@
 """Main entry point into the dataprepkit package
 """
-from dataprepkit.helpers.connectors.warehouse import (
-    get_fabric_warehouse_engine,
-    validate_fabric_warehouse_engine
-)
+import dataprepkit.helpers.connectors.warehouse
+import dataprepkit.processors.dimensions.dim_common
