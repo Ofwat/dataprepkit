b3486303f851356f1ffad7d55ef1bf856c9b1334
coau <79207858+coau@users.noreply.github.com>
2026-02-18 12:59:03 +0000

    Enable fast_executemany by default for Fabric

--- dataprepkit/helpers/connectors/fabric.py
+++ dataprepkit/helpers/connectors/fabric.py
@@ -52,7 +52,7 @@ def create_engine_for_fabric(
     database: str,
     preferred_driver: Optional[str] = None,
     port: int | None = None,
-    fast_executemany: bool = False,
+    fast_executemany: bool = True,
 ) -> sa.engine.Engine:
     driver = _get_driver(preferred_driver) if preferred_driver else _get_driver()
     if credentials is None:
